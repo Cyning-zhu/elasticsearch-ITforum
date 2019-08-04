6608156ab6ddab438be9f99f26d7b2b4fd5842f8
zxl <483533120@qq.com>
2019-08-04 17:12:11 +0800

    project ready!

---
+++ src/main/java/com/es/learn/ItFroumApplication.java
@@ -0,0 +1,13 @@
+package com.es.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItFroumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItFroumApplication.class, args);
+	}
+
+}
